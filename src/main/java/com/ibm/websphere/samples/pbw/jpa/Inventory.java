0c7455085f9a37ec29bf8b3205c5d98588982ade
David Carew <carew@us.ibm.com>
2019-10-09 15:28:49 -0500

    Removed schema from JPA entities

--- src/main/java/com/ibm/websphere/samples/pbw/jpa/Inventory.java
+++ src/main/java/com/ibm/websphere/samples/pbw/jpa/Inventory.java
@@ -1,15 +1,15 @@
 //
-// COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, 
-// modify, and distribute these sample programs in any form without payment to IBM for the purposes of 
-// developing, using, marketing or distributing application programs conforming to the application 
-// programming interface for the operating platform for which the sample code is written. 
-// Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS 
-// AND IBM DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED 
-// WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR PURPOSE, 
-// TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE 
-// SAMPLE SOURCE CODE. IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS 
-// OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.  
+// COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy,
+// modify, and distribute these sample programs in any form without payment to IBM for the purposes of
+// developing, using, marketing or distributing application programs conforming to the application
+// programming interface for the operating platform for which the sample code is written.
+// Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS
+// AND IBM DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR PURPOSE,
+// TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE
+// SAMPLE SOURCE CODE. IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS
+// OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
 //
 // (C) COPYRIGHT International Business Machines Corp., 2001,2011
 // All Rights Reserved * Licensed Materials - Property of IBM
@@ -29,11 +29,11 @@ import com.ibm.websphere.samples.pbw.utils.Util;
 /**
  * Inventory is the bean mapping for the INVENTORY table. It provides information about products the
  * store has for sale.
- * 
+ *
  * @see Inventory
  */
 @Entity(name = "Inventory")
-@Table(name = "INVENTORY", schema = "APP")
+@Table(name = "INVENTORY")
 @NamedQueries({
 		@NamedQuery(name = "getItemsByCategory", query = "select i from Inventory i where i.category = :category ORDER BY i.inventoryId"),
 		@NamedQuery(name = "getItemsLikeName", query = "select i from Inventory i where i.name like :name"),
@@ -58,7 +58,7 @@ public class Inventory implements Cloneable, java.io.Serializable {
 	private boolean isPublic;
 	private int minThreshold;
 	private int maxThreshold;
-	
+
 	@Version
 	private long version;
 
@@ -144,7 +144,7 @@ public class Inventory implements Cloneable, java.io.Serializable {
 
 	/**
 	 * Increase the quantity of this inventory item.
-	 * 
+	 *
 	 * @param quantity
 	 *            The number to increase the inventory by.
 	 */
@@ -259,7 +259,7 @@ public class Inventory implements Cloneable, java.io.Serializable {
 	/**
 	 * Same as getInventoryId. Added for compatability with ShoppingCartItem when used by the Client
 	 * XJB sample
-	 * 
+	 *
 	 * @return String ID of the inventory item
 	 */
 	public String getID() {
@@ -269,7 +269,7 @@ public class Inventory implements Cloneable, java.io.Serializable {
 	/**
 	 * Same as setInventoryId. Added for compatability with ShoppingCartItem when used by the Client
 	 * XJB sample
-	 * 
+	 *
 	 */
 	public void setID(String id) {
 		inventoryId = id;
@@ -303,7 +303,7 @@ public class Inventory implements Cloneable, java.io.Serializable {
 	public void setBackOrder(BackOrder backOrder) {
 		this.backOrder = backOrder;
 	}
-	
+
 	@Override
 	public String toString() {
 	    return getClass().getSimpleName() + "{id=" + inventoryId + "}";
--- src/main/java/com/ibm/websphere/samples/pbw/jpa/OrderItem.java
+++ src/main/java/com/ibm/websphere/samples/pbw/jpa/OrderItem.java
@@ -1,15 +1,15 @@
 //
-// COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, 
-// modify, and distribute these sample programs in any form without payment to IBM for the purposes of 
-// developing, using, marketing or distributing application programs conforming to the application 
-// programming interface for the operating platform for which the sample code is written. 
-// Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS 
-// AND IBM DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED 
-// WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR PURPOSE, 
-// TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE 
-// SAMPLE SOURCE CODE. IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS 
-// OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.  
+// COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy,
+// modify, and distribute these sample programs in any form without payment to IBM for the purposes of
+// developing, using, marketing or distributing application programs conforming to the application
+// programming interface for the operating platform for which the sample code is written.
+// Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS
+// AND IBM DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR PURPOSE,
+// TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE
+// SAMPLE SOURCE CODE. IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS
+// OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
 //
 // (C) COPYRIGHT International Business Machines Corp., 2003,2011
 // All Rights Reserved * Licensed Materials - Property of IBM
@@ -33,12 +33,12 @@ import com.ibm.websphere.samples.pbw.utils.Util;
  * Bean mapping for the ORDERITEM table.
  */
 @Entity(name = "OrderItem")
-@Table(name = "ORDERITEM", schema = "APP")
+@Table(name = "ORDERITEM")
 @NamedQueries({ @NamedQuery(name = "removeAllOrderItem", query = "delete from OrderItem") })
 public class OrderItem {
 	/**
 	 * Composite Key class for Entity Bean: OrderItem
-	 * 
+	 *
 	 * Key consists of essentially two foreign key relations, but is mapped as foreign keys.
 	 */
 	@Embeddable
@@ -209,7 +209,7 @@ public class OrderItem {
 
 	/**
 	 * Sets the order for this item Also updates the sellDate
-	 * 
+	 *
 	 * @param order
 	 */
 	public void setOrder(Order order) {
